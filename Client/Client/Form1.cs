2659c47f48d86739beb1f5a98a1c5a13762a0425
SimoneCattaneo17 <cattaneo.simone.studente@itispaleocapa.it>
2022-11-04 13:50:13 +0100

    inizio creazione interfaccia

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Backlog : Form
+    {
+        public Backlog()
+        {
+            InitializeComponent();
+        }
+
+        private void Backlog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+//invece di immagini singole provare a usare una imagelist
+
+//quando si clicca sull'icona si apre una nuova schermata (copia da stash)
+
+//bio in markdown/html
+//https://stackoverflow.com/questions/30928/a-wysiwyg-markdown-control-for-windows-forms
+
+//le immagini e i dati dei giochi vengono scaricate e salvate in locale in una cache che a chiusura del programma viene cancellata totalmente
